847edbee7fb5a4692e805f4bb4802dc8ff701075
Aaron Klotnia <aklotnia@gmail.com>
2019-10-15 00:18:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
+  emoji_library = load_library(file_path)
   # code goes here
 end
